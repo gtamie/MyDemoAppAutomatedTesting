4c983ce537fba8ffb33430919f5d07202609fa23
gtamie <giulianatamie@gmail.com>
2026-05-26 17:04:57 -0300

    fixed methods from menu page

--- pages/menu_page.py
+++ pages/menu_page.py
@@ -2,6 +2,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class MenuPage:
 
     MENU_ICON = (AppiumBy.ACCESSIBILITY_ID, "View menu")
@@ -13,8 +14,8 @@ class MenuPage:
         self.wait = WebDriverWait(driver, 10)
 
     def open_menu(self):
-        self.wait.until(EC.element_to_be_clickable(*self.MENU_ICON)).click()
+        self.wait.until(EC.element_to_be_clickable(self.MENU_ICON)).click()
 
     def go_to_login(self):
         self.open_menu()
-        self.wait.until(EC.element_to_be_clickable(*self.LOGIN_ITEM)).click()
+        self.wait.until(EC.element_to_be_clickable(self.LOGIN_ITEM)).click()
